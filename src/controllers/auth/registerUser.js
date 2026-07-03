a04a30e548b45bb0614446d2660f7e33ec64af1e
Erik Avalian <avalyan.ervand@gmail.com>
2026-07-03 18:16:29 +0200

    Add user registration endpoint

--- src/controllers/auth/registerUser.js
+++ src/controllers/auth/registerUser.js
@@ -1,3 +1,14 @@
-export const registerUser = async (req, res) => {
-  res.json({ message: "User registered successfully" });
+import { authService } from "../../services/index.js";
+
+export const registerUser = async (req, res, next) => {
+  try {
+    const user = await authService.registerUser(req.body);
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
---
+++ src/services/index.js
@@ -0,0 +1,5 @@
+import { registerUser } from "./authService.js";
+
+export const authService = {
+  registerUser,
+};
