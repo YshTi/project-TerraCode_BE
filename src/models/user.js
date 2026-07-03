e22e4c101673b528634ecb202d915452497b33d2
YushTi <zebra2368@gmail.com>
2026-07-03 16:35:52 +0200

    added models

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,56 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    avatarUrl: {
+      type: String,
+      default: "",
+    },
+
+    articlesAmount: {
+      type: Number,
+      default: 0,
+    },
+
+    savedArticles: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Story",
+      },
+    ],
+
+    // Optional
+    email: {
+      type: String,
+      lowercase: true,
+      trim: true,
+    },
+
+    // Optional
+    password: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Unique email only for users that actually have email
+userSchema.index(
+  { email: 1 },
+  {
+    unique: true,
+    partialFilterExpression: {
+      email: { $type: "string" },
+    },
+  }
+);
 
 export const UserModel = model("user", userSchema);
