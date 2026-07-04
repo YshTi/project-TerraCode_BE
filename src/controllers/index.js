9bd7eb52f367238f45ad5520b9eaae56572147cf
MaksOrlenko <144942642+MaksOrlenko@users.noreply.github.com>
2026-07-05 00:56:32 +0300

    feat: implement user login endpoint

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
-import { registerUser } from './auth/registerUser.js';
-import { getCategories } from './categories/getCategories.js';
+import { registerUser } from "./auth/registerUser.js";
+import { loginUser } from "./auth/loginUser.js";
+import { getCategories } from "./categories/getCategories.js";
 import { getCurrentUserStories } from "./users/getCurrentUserStories.js";
 
 export const authController = {
   registerUser,
+  loginUser,
 };
 
 export const categoriesController = {
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 
 import { authController as ctrl } from "../controllers/index.js";
-import { registerUserValidation } from "../validations/index.js";
+import {
+  registerUserValidation,
+  loginUserValidation,
+} from "../validations/index.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", registerUserValidation, ctrl.registerUser);
+authRouter.post("/login", loginUserValidation, ctrl.loginUser);
 
 export default authRouter;
