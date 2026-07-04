1be53493722948f10877e4b1ebac5247fae3b78b
Anna Smyrnova <anyto4ka777s@gmail.com>
2026-07-04 15:46:50 +0300

    add user profile controller and router

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
+// import { getUserValidations} from '../validations/';
+// import { celebrate } from 'celebrate';
+import { getUserProfileController } from '../controllers/users/getUserProfileControllers.js'
+
 
 const usersRouter = Router();
 
+// usersRouter.get('/:id', celebrate(getUserValidations), getUserProfileController); - роут з валідацією
 
+usersRouter.get('/:id', getUserProfileController);
 
 export default usersRouter;
