3d814239e5e8d1b69ab22f72b283bd63d1c9ce8d
Tetiana Yushkevych <139336268+YshTi@users.noreply.github.com>
2026-07-05 21:45:31 +0200

    Reorganize imports in index.js for clarity

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,16 +1,13 @@
 import { registerUser } from "./auth/registerUser.js";
-import { getCategories } from "./categories/getCategories.js";
-import { getCurrentUserStories } from "./users/getCurrentUserStories.js";
-import { getSavedStoriesController } from "./users/getSavedStories.js";
-import { getCurrentUser } from "./users/getCurrentUser.js";
 import { loginUser } from "./auth/loginUser.js";
 import { logoutUser } from "./auth/logoutUser.js";
-
 import { checkSession } from "./auth/checkSession.js";
+
 import { getCategories } from "./categories/getCategories.js";
 
 import { getCurrentUser } from "./users/getCurrentUser.js";
 import { getCurrentUserStories } from "./users/getCurrentUserStories.js";
+import { getSavedStoriesController } from "./users/getSavedStories.js";
 import { addSavedStory, removeSavedStory } from "./users/savedStories.js";
 
 export const authController = {
