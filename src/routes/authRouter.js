fd15760a3d2dccd7af5b1431b567c192b082c8ec
Tetiana Yushkevych <139336268+YshTi@users.noreply.github.com>
2026-07-05 16:19:19 +0200

    Refactor authRouter to include session check

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,12 +2,6 @@ import { Router } from "express";
 import { validationErrorHandler } from "../middleware/validationErrorHandler.js";
 import { authController as ctrl } from "../controllers/index.js";
 import { authenticate } from "../middleware/authenticate.js";
-import { registerUserValidation } from "../validations/index.js";
-
-const authRouter = Router();
-
-authRouter.post("/register", registerUserValidation, ctrl.registerUser);
-authRouter.get("/session", authenticate, ctrl.checkSession);
 import {
   registerUserValidation,
   loginUserValidation,
@@ -27,6 +21,12 @@ authRouter.post(
   loginUserValidation,
   ctrl.loginUser,
   validationErrorHandler,
+);
+
+authRouter.get(
+  "/session",
+  authenticate,
+  ctrl.checkSession,
 );
 
 export default authRouter;
