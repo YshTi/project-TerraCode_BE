f036226506514f5da0370062fdb1f78362c569e6
Larimar4you <Zajanezlaja@gmail.com>
2026-07-05 10:06:03 +0200

    saved stories edge cases

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,6 @@
 import createError from "http-errors";
 import { StoryModel, UserModel } from "../models/index.js";
+import mongoose from "mongoose";
 
 export const getCurrentUserStories = async ({
   userId,
@@ -39,6 +40,10 @@ export const getCurrentUserStories = async ({
 };
 
 export const addStoryToSaved = async ({ userId, storyId }) => {
+  if (!mongoose.Types.ObjectId.isValid(storyId)) {
+    throw createError(400, "Invalid story id format");
+  }
+
   const story = await StoryModel.findById(storyId);
 
   if (!story) {
@@ -59,21 +64,35 @@ export const addStoryToSaved = async ({ userId, storyId }) => {
 };
 
 export const removeStoryFromSaved = async ({ userId, storyId }) => {
+  if (!mongoose.Types.ObjectId.isValid(storyId)) {
+    throw createError(400, "Invalid story id format");
+  }
+
   const story = await StoryModel.findById(storyId);
 
   if (!story) {
     throw createError(404, "Story not found");
   }
 
-  const user = await UserModel.findByIdAndUpdate(
-    userId,
-    { $pull: { savedArticles: storyId } },
-    { new: true },
-  ).select("-password");
+  const user = await UserModel.findById(userId).select("savedArticles");
 
   if (!user) {
     throw createError(404, "User not found");
   }
 
-  return user;
+  const isSaved = user.savedArticles.some(
+    (savedStoryId) => savedStoryId.toString() === storyId,
+  );
+
+  if (!isSaved) {
+    throw createError(404, "Story is not in saved stories");
+  }
+
+  const updatedUser = await UserModel.findByIdAndUpdate(
+    userId,
+    { $pull: { savedArticles: storyId } },
+    { new: true },
+  ).select("-password");
+
+  return updatedUser;
 };
