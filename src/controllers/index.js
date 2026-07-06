556166fe5ccbb1856a1568095567bf3a3829d1c8
Tetiana Yushkevych <139336268+YshTi@users.noreply.github.com>
2026-07-06 17:46:56 +0200

    Refactor import statements in index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -9,8 +9,10 @@ import { getCurrentUser } from "./users/getCurrentUser.js";
 import { getCurrentUserStories } from "./users/getCurrentUserStories.js";
 import { getSavedStoriesController } from "./users/getSavedStories.js";
 import { addSavedStory, removeSavedStory } from "./users/savedStories.js";
-import { getCurrentUser } from "./users/getCurrentUser.js";
-import { updateCurrentUser, verifyEmailChange } from "./users/updateCurrentUser.js";
+import {
+  updateCurrentUser,
+  verifyEmailChange,
+} from "./users/updateCurrentUser.js";
 
 export const authController = {
   registerUser,
