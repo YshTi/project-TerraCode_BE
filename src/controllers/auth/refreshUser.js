fd899ada8d2217237b1e65f7ad10234e32ea6428
v.sementsov <v.sementsov@andersenlab.com>
2026-07-08 20:51:07 +0200

    add refreshToken

---
+++ src/controllers/auth/refreshUser.js
@@ -0,0 +1,13 @@
+export const refreshUser = async (req, res, next) => {
+    try {
+        const { refreshToken } = req.cookies;
+
+        const accessToken = await authService.refreshUserToken(refreshToken);
+
+        res.status(200).json({
+            accessToken,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
