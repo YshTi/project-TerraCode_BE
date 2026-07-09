0e3116dc755286fafe028daae382836744498265
Tetiana Yushkevych <139336268+YshTi@users.noreply.github.com>
2026-07-09 14:36:49 +0200

    Refactor usersRouter.js to clean up imports
    Removed duplicate import of getUserProfileController and unnecessary userIdValidation import.

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,13 +2,8 @@ import { Router } from "express";
 
 import { usersController as ctrl } from "../controllers/index.js";
 import { authenticate } from "../middleware/authenticate.js";
-import {
-  getCurrentUserStoriesValidation,
-  userIdValidation,
-} from "../validations/index.js";
 import { getUserProfileController } from "../controllers/users/getUserProfileControllers.js";
 import { upload } from "../middleware/multer.js";
-import { getUserProfileController } from "../controllers/users/getUserProfileControllers.js";
 import {
   getCurrentUserStoriesValidation,
   updateCurrentUserValidation,
