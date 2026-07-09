9fbbdc29598624fbf31ad45e4a7d6c7624bb6064
Tetiana Yushkevych <139336268+YshTi@users.noreply.github.com>
2026-07-09 14:55:23 +0200

    Enhance updateAvatar with error handling and response
    Refactor updateAvatar function to include error handling and improve response structure. Also changed name of the updated slot: it is "avatarUrl", not just avatar

--- src/controllers/users/updateAvatar.js
+++ src/controllers/users/updateAvatar.js
@@ -2,19 +2,40 @@ import createHttpError from "http-errors";
 import { UserModel } from "../../models/user.js";
 import { saveFileToCloudinary } from "../../utils/saveFilesToCloudinary.js";
 
-export const updateAvatar = async (req, res) => {
-  const { file, user } = req;
-  if (!file) {
-    throw createHttpError(400, "No file");
+export const updateAvatar = async (req, res, next) => {
+  try {
+    const { file, user } = req;
+
+    if (!file) {
+      throw createHttpError(400, "Avatar file is required");
+    }
+
+    const result = await saveFileToCloudinary(file.buffer, user._id);
+
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      user._id,
+      { avatarUrl: result.secure_url },
+      { new: true },
+    );
+
+    if (!updatedUser) {
+      throw createHttpError(404, "User not found");
+    }
+
+    res.status(200).json({
+      message: "Avatar updated successfully",
+      user: {
+        id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        avatarUrl: updatedUser.avatarUrl,
+        articlesAmount: updatedUser.articlesAmount,
+        savedArticles: updatedUser.savedArticles,
+        createdAt: updatedUser.createdAt,
+        updatedAt: updatedUser.updatedAt,
+      },
+    });
+  } catch (error) {
+    next(error);
   }
-
-  const result = await saveFileToCloudinary(file.buffer, user._id);
-
-  const updatedUser = await UserModel.findOneAndUpdate(
-    { _id: user._id },
-    { avatarUrl: result.secure_url },
-    { returnDocument: "after" },
-  );
-
-  res.status(200).json({ url: user.avatar });
 };
